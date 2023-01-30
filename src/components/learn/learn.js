51b78c6120575cd7193e6cdd4070bc38ecea3337
Wilson Ibekason <wanbekgithub@gmail.com>
2023-01-30 11:23:52 +0100

    create node for orders and ordered Items

--- src/components/learn/learn.js
+++ src/components/learn/learn.js
@@ -26,6 +26,48 @@ class LinkedList {
     }
     this.size++;
   }
+  // 0 (n)
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) return (this.head = node);
+    else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+    this.size++;
+  }
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) return null;
+    let removedNode;
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = tjis.head.node;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removedNode = prev.next;
+      prev.next + removedNode.next;
+    }
+    this.size++;
+    return removedNode;
+  }
+  removeValue(value) {
+    if (this.isEmpty()) {
+      return null;
+    }
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.size++;
+      return value;
+    } else {
+    }
+  }
+  // 0(1)
   print() {
     if (this.isEmpty()) {
       console.log("Nodee is Empty");
@@ -40,12 +82,37 @@ class LinkedList {
       return currentValues;
     }
   }
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index + 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = nide;
+      this.size++;
+    }
+  }
 }
 
 const list = new LinkedList();
 
 console.log(list.isEmpty(), list.getSize(), list.prepend(20));
-console.table(list.prepend(30), list.print(), list.prepend(40), list.print());
+console.table(
+  list.prepend(30),
+  list.print(),
+  list.prepend(40),
+  list.print(),
+  list.insert(30, 30),
+  list.print(),
+  list.removeFrom(10)
+);
 
 class Trapezium {
   constructor(_height, _base, _length, _width) {
@@ -68,3 +135,94 @@ const trapezium = new Trapezium();
 const dimensions = trapezium.getDimensions();
 
 console.log("Response", dimensions);
+
+/// GET CHILD DOM TO BE DISPLAYED TO THE APPLICATION
+const children = document.getElementByID("schoolclass");
+
+class Classes {
+  constructor(_students, _chairs, _tables, _ageGrade) {
+    this.students = _students;
+    this.tables = _tables;
+    this.ageGrade = _ageGrade;
+    this.class = _chairs ? _chairs : null;
+    // each class is represented as a Node and implemenrts tbe total number of students in the class
+  }
+  getClassNodes() {
+    return Math.floor(Math.round(this.student));
+  }
+}
+
+class MyEnvironment {
+  constructor(
+    _ages,
+    _classes,
+    modules,
+    _staffs,
+    _teachers,
+    _parents,
+    _Guests,
+    _security,
+    _others
+  ) {
+    this.ages = _ages;
+    this.classes = _classes;
+    this.staffs = _staffs;
+    this.teachers = _teachers;
+    this.guests = _Guests;
+    this.security = _security;
+    this.others = _others;
+    this.mainResultedPreference = {};
+  }
+  getNumber() {
+    const { ages, classes, staffs, teachers, guests, others } = this;
+    const result = null;
+  }
+  isLoading() {}
+  isError() {}
+  isErrorLoading() {}
+  getClassNode() {}
+  getTotalClasses() {}
+  getConservedListEndPoints() {
+    let lists = [];
+    for (let i = 0; i <= i + lists.length; i++) {
+      if (isLoading()) return null && getClassNode();
+      else getTotalCLasses();
+    }
+  }
+}
+
+const myEnvironment = new MyEnvironment(10, 20);
+console.table(myEnvironment.getNumber());
+//Array.prototype.slice.call
+var __makeTemplateObject =
+  (this && this.__makeTemplateObject) ||
+  function (cooked, raw) {
+    if (Object.defineProperty) {
+      Object.defineProperty(cooked, "raw", { value: raw });
+    } else {
+      cooked.raw = raw;
+    }
+    return cooked;
+  };
+const array = [];
+
+class OrderedList {
+  constructor(_locationPopulate) {
+    (this.location = _locationPopulate),
+      (this.locations = this.locations),
+      (this.producers = []);
+  }
+  getLocation() {
+    let node = null;
+    if (typeof node !== null && !this.location) return (node = this.locations);
+  }
+}
+
+const constructOrder = new OrderedList({
+  name: "wilson",
+  age: 40,
+  title: "Becoming SuperNatural".toString(),
+  level: "30%",
+});
+
+// append add a new Node at the End of the List
