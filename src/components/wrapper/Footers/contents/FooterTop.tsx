02d42d472ba42f484924062316cfb3c56efba67b
Wilson Ibekason <wanbekgithub@gmail.com>
2023-02-14 17:25:38 +0100

    complete add Footer component

--- src/components/wrapper/Footers/contents/FooterTop.tsx
+++ src/components/wrapper/Footers/contents/FooterTop.tsx
@@ -20,8 +20,20 @@ import {
 import { icons } from "../../../../assets/images";
 import { CheckIcon } from "@chakra-ui/icons";
 import { SocialIcons } from "../../../../utils/data/data";
+import { AiFillExclamationCircle, AiTwotoneMessage } from "react-icons/ai";
+import Map, { Marker, Alignment } from "react-map-gl";
+import maplibregl from "maplibre-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const FooterTop = () => {
+  const [viewState, setViewState] = React.useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14,
+  });
+  const MAPBOX_TOKEN =
+    "pk.eyJ1Ijoid2lsaTEyMyIsImEiOiJja3cwaHR4ZDEwZng4MndwcTB4am9iY2kwIn0.4SciK8ia-VplCgi8zQAw6Q";
+
   return (
     <>
       <Box
@@ -130,7 +142,7 @@ const FooterTop = () => {
             </Box>
           </Box>
           {/* Content Container */}
-          <Box w={"full"} maxW={"65%"}>
+          <Box w={"full"} maxW={"65%"} px={8}>
             <Flex
               w={"full"}
               direction={"column"}
@@ -190,19 +202,66 @@ const FooterTop = () => {
                       h={"auto"}
                       bgColor={"transparent"}
                       justify={"center"}
-                      fontSize={"lg"}
+                      fontSize={"md"}
                       letterSpacing={"wide"}
                       fontWeight={"medium"}
                       color={"gray.300"}
+                      experimental_spaceY={10}
                     >
-                      <Text fontSize={"2xl"} mt={10}>
-                        Opening Hours
-                      </Text>
-                      {Array(3)
-                        .fill(0)
-                        .map((_, i) => (
-                          <Text>Mon-9am. Sat-10pm</Text>
-                        ))}
+                      <Flex
+                        direction={"column"}
+                        justify={"center"}
+                        experimental_spaceY={10}
+                      >
+                        <Text fontSize={"xl"}>
+                          Opening Hours
+                        </Text>
+                        {Array(3)
+                          .fill(0)
+                          .map((_, i) => (
+                            <Text>Mon-9am. Sat-10pm</Text>
+                          ))}
+                        <Flex
+                          direction={"row"}
+                          align={"center"}
+                          experimental_spaceX={10}
+                        >
+                          <AiTwotoneMessage />
+                          <Text
+                            textUnderlineOffset={"revert"}
+                            textDecoration={"underline"}
+                          >
+                            08139133401
+                          </Text>
+                        </Flex>
+                      </Flex>
+
+                      <Box
+                        w={"full"}
+                        h={"150px"}
+                        bgColor={"white"}
+                        rounded={"lg"}
+                      >
+                        <Map
+                          {...viewState}
+                          onMove={(evt) => setViewState(evt.viewState)}
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                            borderRadius: "inherit",
+                          }}
+                          mapStyle="mapbox://styles/mapbox/streets-v9"
+                          mapboxAccessToken={MAPBOX_TOKEN}
+
+                          // mapLib={maplibregl}
+                        >
+                          <Marker
+                            longitude={-122.4}
+                            latitude={37.8}
+                            color="red"
+                          />
+                        </Map>
+                      </Box>
                     </Flex>
                   ))}
               </Flex>
